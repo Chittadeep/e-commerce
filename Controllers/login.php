e8fc1c3b6151f3a55c4d07df98b350d807ea7169
Chittadeep Biswas <mailchittadeep@gmail.com>
2021-08-08 12:32:52 +0530

    Created the login system

--- Controllers/login.php
+++ Controllers/login.php
@@ -1,5 +1,30 @@
 <?php
-require_once "config.php"
+require_once "config.php";
 
+$mail = $_POST['e-mail'];
+$password = $_POST['password'];
+
+
+$sql = "SELECT * FROM `Admins` WHERE EMAIL = ? AND PASSWORD = ?";
+$stmt = mysqli_prepare($con, $sql);
+
+
+mysqli_stmt_bind_param($stmt, 'ss', $mail, $password);
+
+
+mysqli_stmt_execute($stmt);
+
+$result = mysqli_stmt_get_result($stmt);
+
+$row = mysqli_fetch_row($result);
+
+$count = mysqli_num_rows($result);
+
+if($count == 1){
+    header("location: ../View/admin.php");
+}
+else{
+    echo "failed logging in";
+}
 
 ?>
